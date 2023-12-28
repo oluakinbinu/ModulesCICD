cb1cf3566d51bf20ab261461c62ddb1d6f9c4ca5
oluakinbinu <154087956+oluakinbinu@users.noreply.github.com>
2023-12-28 15:42:48 -0500

    Update 03-backend.tf

--- 03-backend.tf
+++ 03-backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
     # replace with your bucket name for your state file location 
-    bucket = "ado-user-class5-1231230120i312i31"
+    bucket = "BUCKET NAME"
     # replace with what you want to name your state file
-    key = "moduleexamplestatefile.tfstate"
+    key = "STATE FILE NAME"
     # replace with the region of the bucket
     region = "eu-west-1"
   
